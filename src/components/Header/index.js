dea90abecbc4fb19896efc2745796719ebb155ae
Jamey0621 <you@example.com>
2021-07-09 08:19:07 -0500

    The MovieDB

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+
+const Header = () => (
+  <Wrapper>
+    <Content>
+      <Link to='/'>
+       
+      </Link>
+      
+    </Content>
+  </Wrapper>
+);
+
+export default Header;
